89a25dc4ba75708ba166ac1045ac36aed4a2905e
azamilink <zamiaswad@gmail.com>
2025-10-13 15:41:48 +0700

    install package

--- types/authjs.d.ts
+++ types/authjs.d.ts
@@ -1,4 +1,5 @@
 import { type DefaultSession } from 'next-auth';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {  JWT } from 'next-auth/jwt';
 
 declare module 'next-auth' {
