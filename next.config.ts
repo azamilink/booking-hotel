367ea5c575e785a9bff1ba520203a6105acdb74e
azamilink <zamiaswad@gmail.com>
2025-10-13 19:52:23 +0700

    install ulang node module

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [new URL('https://lh3.googleusercontent.com/**/**')],
-    },
+	images: {
+		remotePatterns: [new URL('https://lh3.googleusercontent.com/**/**')],
+	},
 };
 
 export default nextConfig;
