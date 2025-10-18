d517531d02587adc532073797d821a957d2c7672
azamilink <zamiaswad@gmail.com>
2025-10-19 00:54:20 +0700

    akhirnya

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [new URL('https://lh3.googleusercontent.com/**/**')],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "q8qcqktyq7o1ni37.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
