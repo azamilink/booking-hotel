7072f382ce832faaf5e22ab4c8adea1801352453
azamilink <zamiaswad@gmail.com>
2025-10-20 00:26:32 +0700

    npmrc

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,25 +1,23 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import Navlink from './Navlink';
+import Image from "next/image";
+import Link from "next/link";
+import Navlink from "./Navlink";
 
 export default function Navbar() {
-    return (
-        <div className='fixed top-0 w-full bg-white shadow-sm z-20'>
-            <div className='max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4'>
-                <Link
-                    href='/'
-                    className='flex items-center'>
-                    <Image
-                        src='/logo.png'
-                        className='h-8 mr-3'
-                        alt='Logo'
-                        width={128}
-                        height={49}
-                        priority
-                    />
-                </Link>
-                <Navlink />
-            </div>
-        </div>
-    );
+  return (
+    <div className="fixed top-0 w-full bg-white shadow-sm z-20">
+      <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/logo.png"
+            className="h-8 w-auto mr-3"
+            alt="Logo"
+            width={128}
+            height={49}
+            priority
+          />
+        </Link>
+        <Navlink />
+      </div>
+    </div>
+  );
 }
